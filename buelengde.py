48381d9db3af18abf283690c0f88dea6ac925a3e
sigurdht <legotull.99@gmail.com>
2019-10-22 08:58:14 +0200

    Update buelengde.py
    Nå tar den faktisk det nærmeste punktet og ikke bare den rett før

--- buelengde.py
+++ buelengde.py
@@ -7,13 +7,17 @@ Created on Tue Oct  8 11:25:21 2019
 
 import numpy as np
 
-def getI(buelengde, x_interp, y_interp, alpha):
+def getIndex(buelengde, x_interp, y_interp, alpha):
     s = 0
     i = 0
-    while s < buelengde:
-        i += 1
-        s += np.sqrt((x_interp[i]-x_interp[i-1])**2 + (y_interp[i]-y_interp[i-1])**2)
+    while s < buelengde and i < len(x_interp) - 1:
+        sNew = s + np.sqrt((x_interp[i+1]-x_interp[i])**2 + (y_interp[i+1]-y_interp[i])**2)
+        if abs(sNew - buelengde) < abs(s - buelengde):
+            i += 1
+            s = sNew
+        else:
+            break
+        print(s, buelengde)
     
     return i
 
-#print(getI(0.22, x_interp, y_interp, alpha))
